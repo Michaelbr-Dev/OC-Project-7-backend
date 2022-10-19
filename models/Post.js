0a9614e8ba518ae6e4b7b493842c11bbb06c02a5
Michael Briquet <michael.br@free.fr>
2022-10-19 09:40:19 +0200

    🏗️ Update pots model datas to store in DB

--- models/Post.js
+++ models/Post.js
@@ -5,13 +5,15 @@
 
 const mongoose = require('mongoose');
 
-const postSchema = mongoose.Schema({
-  username: { type: String, required: true },
-  userId: { type: String, required: true },
-  content: { type: String, maxlenght: 500 },
-  attachement: { type: String, required: false },
-  likes: { type: Number, required: false },
-  usersLiked: { type: Array, required: true },
-});
+const postSchema = mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    content: { type: String, maxlenght: 1000, required: false },
+    attachement: { type: String, required: false },
+    likes: { type: Number, required: false },
+    usersLiked: { type: Array, required: true },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('Post', postSchema);
